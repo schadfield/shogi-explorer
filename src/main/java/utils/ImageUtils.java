6cc6f7bb790732ecbd16df66e6cd25d219ad5442
Stephen Chadfield <stephen@chadfield.com>
2021-08-29 11:20:55 +0900

    GUI improvements (?).

--- src/main/java/utils/ImageUtils.java
+++ src/main/java/utils/ImageUtils.java
@@ -41,7 +41,7 @@ public class ImageUtils {
                 rh.add(new RenderingHints(RenderingHints.KEY_ALPHA_INTERPOLATION,
                         RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY));
                 rh.add(new RenderingHints(RenderingHints.KEY_INTERPOLATION,
-                        RenderingHints.VALUE_INTERPOLATION_BICUBIC));
+                        RenderingHints.VALUE_INTERPOLATION_BILINEAR));
                 rh.add(new RenderingHints(RenderingHints.KEY_ANTIALIASING,
                         RenderingHints.VALUE_ANTIALIAS_ON));
                 rh.add(new RenderingHints(RenderingHints.KEY_COLOR_RENDERING,
@@ -51,7 +51,7 @@ public class ImageUtils {
                 rh.add(new RenderingHints(RenderingHints.KEY_RENDERING,
                         RenderingHints.VALUE_RENDER_QUALITY));
                 rh.add(new RenderingHints(RenderingHints.KEY_STROKE_CONTROL,
-                        RenderingHints.VALUE_STROKE_PURE));
+                        RenderingHints.VALUE_STROKE_NORMALIZE));
                 rh.add(new RenderingHints(RenderingHints.KEY_FRACTIONALMETRICS,
                         RenderingHints.VALUE_FRACTIONALMETRICS_ON));
                 rh.add(new RenderingHints(RenderingHints.KEY_TEXT_ANTIALIASING,
