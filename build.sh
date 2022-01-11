401cfbacdf6d0b10988a13cb956234242cd37bf0
Stephen Chadfield <stephen@chadfield.com>
2022-01-11 15:23:38 +0900

    Release update.

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ cp /Users/stephen/Projects/Releases/Files/V2/ShogiExplorer.jar \
   /Users/stephen/Projects/Releases/Templates/V2/Shogi\ Explorer.app/Contents/Java/ShogiExplorer.jar
 cp -pr /Users/stephen/Projects/Releases/Templates/V2/Shogi\ Explorer.app \
   /Users/stephen/Projects/Releases/Files/V2
+  touch /Users/stephen/Projects/Releases/Files/V2/Shogi\ Explorer.app
