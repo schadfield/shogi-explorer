13940b3fed8fa1786a8236ee94e11ac461c35273
Stephen Chadfield <stephen@chadfield.com>
2022-01-11 21:48:38 +0900

    Version 2.0.2: faster board drawing.

--- src/main/java/com/chadfield/shogiexplorer/utils/ImageUtils.java
+++ src/main/java/com/chadfield/shogiexplorer/utils/ImageUtils.java
@@ -80,23 +80,7 @@ public class ImageUtils {
 
     public static BufferedImage transcodeSVGToBufferedImage(InputStream inputStream, double width) throws TranscoderException {
         // Create a PNG transcoder.
-        PNGTranscoder t = new PNGTranscoder() {
-            @Override
-            protected ImageRenderer createRenderer() {
-                ImageRenderer r = super.createRenderer();
-                RenderingHints rh = r.getRenderingHints();
-                rh.add(new RenderingHints(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY));
-                rh.add(new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON));
-                rh.add(new RenderingHints(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY));
-                rh.add(new RenderingHints(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_DISABLE));
-                rh.add(new RenderingHints(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON));
-                rh.add(new RenderingHints(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC));
-                rh.add(new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY));
-                rh.add(new RenderingHints(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE));
-                rh.add(new RenderingHints(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_OFF));
-                return r;
-            }
-        };
+        PNGTranscoder t = new PNGTranscoder();
 
         // Set the transcoding hints.
         t.addTranscodingHint(SVGAbstractTranscoder.KEY_WIDTH, (float) width);
